03344d23cc294511c483baf255566379d3e8e634
erick-costa <erickcosta2509@gmail.com>
2024-05-10 11:58:50 -0300

    correções css DeletarCategoria

--- src/components/categorias/deletarCategoria/DeletarCategoria.tsx
+++ src/components/categorias/deletarCategoria/DeletarCategoria.tsx
@@ -40,15 +40,15 @@ function DeletarCategoria() {
     retornar()
   }
   return (
-    <div className="container w-1/3 mx-auto">
+    <div className="container w-1/3 mx-auto my-10">
       <h1 className="text-4xl text-center my-4">Deletar categoria</h1>
 
       <p className="text-center font-semibold mb-4">
         Você tem certeza de que deseja apagar a categoria a seguir?
       </p>
 
-      <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
-        <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
+      <div className="border flex flex-col rounded-md shadow overflow-hidden justify-between">
+        <header className="py-2 px-6 bg-green-600 text-white font-bold text-2xl">
           Categoria
         </header>
         <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.nome}</p>
@@ -60,7 +60,7 @@ function DeletarCategoria() {
             Não
           </button>
           <button
-            className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center"
+            className="w-full text-slate-100 bg-green-400 hover:bg-green-600 flex items-center justify-center"
             onClick={deletarCategoria}
           >
             Sim
